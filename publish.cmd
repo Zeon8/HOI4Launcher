fc5acd28b0f8ccc2eb362593b9a30467767fab0d
Zeon8 <dimayaremko@ukr.net>
2023-03-03 15:08:16 +0200

    update publish file

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,2 @@
-dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true --no-self-contained
+dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true --self-contained
+dotnet publish -r osx-x64 -c Release -p:PublishSingleFile=true --self-contained
